1832715b5dd7fe0473c9eeb5e85d28076c6de9f3
Akshara Sundararajan <akshara-sun@users.noreply.github.com>
2021-03-26 12:38:34 -0700

    updated styles for sequencer

--- src/sequencer.js
+++ src/sequencer.js
@@ -7,12 +7,15 @@ export default function Sequencer () {
         <>
          <h1 className="font-mono text-center text-6xl p-10">NSequencer</h1>
         <div className="grid grid-flow-col grid-cols-3 grid-rows-2 justify-items-stretch gap-4 m-10">
-            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-100 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">1</div>
-            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-100 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">2</div>
-            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-100 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">3</div>
-            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-100 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">4</div>
-            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-100 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">5</div>
-            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-100 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">6</div>
+            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-800 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">Hi Tom
+            <audio autoPlay="false">
+            </audio>
+            </div>
+            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-800 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">Mid Tom</div>
+            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-800 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">Hi-Hat</div>
+            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-800 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">Kick</div>
+            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-800 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">Snare</div>
+            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-800 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">Crash</div>
         </div>
         </>
     )
