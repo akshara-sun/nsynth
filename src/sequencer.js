371e6243bc8faff91f2d8dbc7467f576a28fb5b7
Akshara Sundararajan <akshara-sun@users.noreply.github.com>
2021-03-27 13:08:40 -0700

    sequencer can make noise on click

--- src/sequencer.js
+++ src/sequencer.js
@@ -1,23 +1,26 @@
 import React from "react";
 import "./styles.css";
 import * as Tone from "tone";
-import { ToneEvent } from "tone";
 
 export default function Sequencer () {
     const sequencerSounds = {
-       1: new Tone.AMSynth().toDestination(),
-       2: new Tone.FMSynth().toDestination(),
-       3: new Tone.DuoSynth().toDestination(),
-       4: new Tone.MembraneSynth().toDestination(),
-       5: new Tone.MetalSynth().toDestination(),
-       6: new Tone.PluckSynth().toDestination(),
+       AM: new Tone.AMSynth().toDestination(),
+       DUO: new Tone.DuoSynth().toDestination(),
+       MEMBRANE: new Tone.MembraneSynth().toDestination(),
+       FM: new Tone.FMSynth().toDestination(),
+       PLUCK: new Tone.PluckSynth().toDestination(),
+       POLY: new Tone.PolySynth().toDestination(),
+    }
+
+    const makeSound = (synthType) => {
+        sequencerSounds[synthType].triggerAttackRelease("C3", "8n")
     }
 
     return (
         <>
          <h1 className="font-mono text-center text-6xl p-10">NSequencer</h1>
         <div className="grid grid-flow-col grid-cols-3 grid-rows-2 justify-items-stretch gap-4 m-10">
-            {Object.keys(sequencerSounds).map(sound => <button className="text-center font-mono font-bold border-2 border-black bg-yellow-800 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">{sound}</button>)}
+            {Object.keys(sequencerSounds).map(sound => <button onClick={() => makeSound(sound)}className="text-center font-mono font-bold border-2 border-black bg-yellow-800 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">{sound}</button>)}
         </div>
         </>
     )
