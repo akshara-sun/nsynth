4e0470a05d4be868dc7c77f4a02485e510c69b52
Akshara Sundararajan <akshara-sun@users.noreply.github.com>
2021-03-27 12:22:42 -0700

    Implementing Tone JS for sequencer

--- src/sequencer.js
+++ src/sequencer.js
@@ -1,21 +1,23 @@
 import React from "react";
 import "./styles.css";
-
+import * as Tone from "tone";
+import { ToneEvent } from "tone";
 
 export default function Sequencer () {
+    const sequencerSounds = {
+       1: new Tone.AMSynth().toDestination(),
+       2: new Tone.FMSynth().toDestination(),
+       3: new Tone.DuoSynth().toDestination(),
+       4: new Tone.MembraneSynth().toDestination(),
+       5: new Tone.MetalSynth().toDestination(),
+       6: new Tone.PluckSynth().toDestination(),
+    }
+
     return (
         <>
          <h1 className="font-mono text-center text-6xl p-10">NSequencer</h1>
         <div className="grid grid-flow-col grid-cols-3 grid-rows-2 justify-items-stretch gap-4 m-10">
-            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-800 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">Hi Tom
-            <audio autoPlay="false">
-            </audio>
-            </div>
-            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-800 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">Mid Tom</div>
-            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-800 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">Hi-Hat</div>
-            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-800 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">Kick</div>
-            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-800 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">Snare</div>
-            <div className="text-center font-mono font-bold border-2 border-black bg-yellow-800 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">Crash</div>
+            {Object.keys(sequencerSounds).map(sound => <button className="text-center font-mono font-bold border-2 border-black bg-yellow-800 p-20 hover:bg-black hover:border-yellow-100 hover:text-white focus:outline-none shadow-xl">{sound}</button>)}
         </div>
         </>
     )
