c79f2d7525f4c8f18a31673c4cf46b4b24572102
Akshara Sundararajan <akshara-sun@users.noreply.github.com>
2021-11-09 16:34:40 -0800

    archhitectural changes

--- src/Synth.js
+++ src/Synth.js
@@ -1,91 +1,83 @@
+
 import React from "react";
+import { useEffect , useRef} from "react";
 import "./Synth.css";
 import * as Tone from "tone";
-// import OctaveUp from "./octave-up";
-// import OctaveDown from "./octave-down";
 
-//Synth component with black and white keys
-export default function Synth () {
-  let keyboard = [
-    "C4",
-    "C#4",
-    "D4",
-    "D#4",
-    "E4",
-    "F4",
-    "F#4",
-    "G4",
-    "G#4",
-    "A4",
-    "A#4",
-    "B4",
-    "C5",
-    "C#5",
-    "D5",
-    "D#5",
-    "E5",
-    "F5",
-    "F#5",
-    "G5",
-    "G#5",
-    "A5",
-    "A#5",
-    "B5",
-  ]
-//white keys of the keyboard
-  let whiteKeys = keyboard.filter(notes => !notes.includes("#"))
-
-  //black keys of the keyboard
-  let blackKeysSet1 = [
-    "C#4",
-    "D#4",
-  ];
-
-  let blackKeysSet2 = [
-    "F#4",
-    "G#4",
-    "A#4"
-  ]
-
-  let blackKeysSet3 = [
-    "C#5",
-    "D#5",
-  ];
-
-  let blackKeysSet4 = [
-    "F#5",
-    "G#5",
-    "A#5"
-  ]
-
-  //creating synth and connecting to main output
-  const synth = new Tone.Synth().toDestination();
-
-  //function that will take a note and play said note for a duration of an 8th note
-  const playNote = (note) => {
-    synth.triggerAttackRelease(`${note}`, "8n");
+export default function pianoKeys() {
+  let keyboard = {
+    "a": "C4",
+    "w": "C#4",
+    "s": "D4",
+    "e": "D#4",
+    "d": "E4",
+    "f": "F4",
+    "t": "F#4",
+    "g": "G4",
+    "y": "G#4",
+    "h": "A4",
+    "u": "A#4",
+    "j": "B4",
+    "k": "C5",
   };
+
+  let keyboardShortcuts = Object.keys(keyboard);
+
+  //function that looks through object values and filters out the white keys and black keys
+  let keyNotes = Object.values(keyboard);
+  let whiteKeys = keyNotes.filter((keys) => !keys.includes("#"));
+
+  let blackKeysSet1 = keyNotes.filter((keys) => keys.includes("C#"));
+  blackKeysSet1.push(keyNotes.filter((keys) => keys.includes("D#")));
+
+  let blackKeysSet2 = keyNotes.filter((keys) => keys.includes("F#"));
+  blackKeysSet2.push(keyNotes.filter((keys) => keys.includes("G#")));
+  blackKeysSet2.push(keyNotes.filter((keys) => keys.includes("A#")));
+
+  //creating piano and connecting to main output source
+  const piano = new Tone.Synth().toDestination();
+
+  //function that takes care of playing a specific sound
+  const playNote = (note) => {
+    piano.triggerAttackRelease(`${note}`, "8n");
+  };  
 
   return (
     <>
-    <h1 className="font-mono text-center text-6xl p-4">NSynth</h1>
-    <div className= "white-keys-styles flex justify-center relative sm:mt-32 md:mt-64">  
-      {whiteKeys.map(note => <ul onClick={() => playNote(note)} id= "whitekeys" className="relative text-center font-mono font-bold bg-white border border-t-8 border-black rounded-b-lg w-16 pt-52 hover:bg-gray-500 shadow-2xl focus:outline-none">{note}</ul>)}
-    </div>
-    <div className="black-keys-styles flex justify-center relative space-x-4 -m-60">
-      {blackKeysSet1.map(note => <ul onClick={() => playNote(note)} id= "blackKeys" className="space-x-4 text-center font-mono font-bold text-white bg-black rounded-b-lg w-12 pt-40 hover:bg-gray-500 focus:outline-none shadow-2xl">{note}</ul>)}
-      <div className="flex justify-start pl-16 space-x-4">
-        {blackKeysSet2.map(note => <ul onClick={() => playNote(note)} id= "blackKeys" className="space-x-4 text-center font-mono font-bold text-white bg-black rounded-b-lg w-12 pt-40 hover:bg-gray-500 focus:outline-none shadow-2xl">{note}</ul>)}
-        <div className="flex justify-start pl-16 space-x-4">
-          {blackKeysSet3.map(note => <ul onClick={() => playNote(note)} id= "blackKeys" className="space-x-4 text-center font-mono font-bold text-white bg-black rounded-b-lg w-12 pt-40 hover:bg-gray-500 focus:outline-none shadow-2xl">{note}</ul>)}
-        </div>
-          <div className="flex justify-start pl-16 space-x-4">
-          {blackKeysSet4.map(note => <ul onClick={() => playNote(note)} id= "blackKeys" className="space-x-4 text-center font-mono font-bold text-white bg-black rounded-b-lg w-12 pt-40 hover:bg-gray-500 focus:outline-none shadow-2xl">{note}</ul>)}
-          </div>
+      <h1 className="font-serif text-center text-6xl p-10">NSynth</h1>
+      <div className="flex justify-center relative sm:mt-24 md:mt-44">
+        {whiteKeys.map((note) => (
+          <button
+            className="relative text-center font-mono font-bold bg-white border border-t-8 border-black rounded-b-lg w-16 pt-52 hover:bg-gray-500 shadow-2xl focus:outline-none"
+            id="whitekeys"
+            onClick={() => playNote(note)}
+          >
+            {note}
+          </button>
+        ))}
       </div>
-    </div>
+      <div className="flex justify-center relative space-x-4 -m-60 mr-2">
+        {blackKeysSet1.map((note) => (
+          <button
+            className="ml-44 space-x-4 text-center font-mono font-bold text-white bg-black rounded-b-lg w-12 pt-40 hover:bg-gray-500 focus:outline-none shadow-2xl"
+            id="blackKeys"
+            onClick={() => playNote(note)}
+          >
+            {note}
+          </button>
+        ))}
+        <div className="flex justify-start pl-16 space-x-4">
+          {blackKeysSet2.map((note) => (
+            <button
+              className="space-x-4 text-center font-mono font-bold text-white bg-black rounded-b-lg w-12 pt-40 hover:bg-gray-500 focus:outline-none shadow-2xl"
+              id="blackKeys"
+              onClick={() => playNote(note)}
+              >
+              {note}
+            </button>
+          ))}
+        </div>
+      </div>
     </>
-  )
+  );
 }
-
-
--- src/octave-down.js
+++ src/octave-down.js
@@ -11,7 +11,6 @@ export default function OctaveDown(arr) {
         }
       } 
       return arr;
-      
 }
 
 
