ac5bfb5bd2da5187de100b300f801255e681d1e8
Akshara Sundararajan <akshara-sun@users.noreply.github.com>
2021-11-12 15:05:08 -0800

    removed keyboard event listeners and cleaned up UI

--- src/components/main.js
+++ src/components/main.js
@@ -23,80 +23,34 @@ export default function Synth() {
     piano.triggerAttackRelease(`${note}`, "8n");
   };
 
-  //Keyboard Event Listeners
-    document.addEventListener("keydown", (event) => {
-      // eslint-disable-next-line default-case
-      switch (event.key) {
-        case "a":
-          playNote('C4');
-          break;
-        case "w":
-          playNote('C#4');
-          break;
-        case "s":
-          playNote('D4');
-          break;
-        case "e":
-          playNote('D#4');
-          break;
-        case "d":
-          playNote('E4');
-          break;
-        case "f":
-          playNote('F4');
-          break;
-        case "t":
-          playNote('F#4');
-          break;
-        case "g":
-          playNote('G4');
-          break;
-        case "y":
-          playNote('G#4');
-          break;
-        case "h":
-          playNote('A4');
-          break;
-        case "u":
-          playNote('A#4');
-          break;
-        case "j":
-          playNote('B4');
-          break;
-        case "k":
-          playNote('C5')
-          break;
-    } 
-  })
-
   return (
     <>
       <h1 className="font-serif text-center text-6xl p-10">NSynth</h1>
       <div className="flex justify-center relative sm:mt-24 md:mt-44">
         {whiteKeys.map((note) => (
           <button
-            className="keys relative text-center font-mono font-bold bg-white border border-t-8 border-black rounded-b-lg w-16 pt-52"
+            className="white relative text-center font-mono font-bold bg-white border border-t-8 border-black rounded-b-lg w-16 pt-52"
             id="whitekeys"
             onClick={() => playNote(note)}
           >
-            {note}
+            {note[0]}
           </button>
         ))}
       </div>
-      <div className="flex justify-center relative space-x-4 -m-60 mr-2">
+      <div className="flex justify-center relative space-x-8 -m-64 pr-12">
         {twoBlackKeys.map((note) => (
           <button
-            className="keys ml-44 space-x-4 text-center font-mono font-bold text-white bg-black rounded-b-lg w-12 pt-40"
+            className="black text-s text-white ml-44 space-x-4 bg-black rounded-b-lg w-12 pt-40"
             id="twoBlackKeys"
             onClick={() => playNote(note)}
           >
             {note}
           </button>
         ))}
-        <div className="flex justify-start pl-16 space-x-4">
+        <div className="flex justify-start pl-20 space-x-8">
           {threeBlackKeys.map((note) => (
             <button
-              className="keys space-x-4 text-center font-mono font-bold text-white bg-black rounded-b-lg w-12 pt-40"
+              className="black text-s text-white space-x-4 bg-black rounded-b-lg w-12 pt-40"
               id="threeBlackKeys"
               onClick={() => playNote(note)}
             >
