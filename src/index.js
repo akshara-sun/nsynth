0c9292cfb6067985909e42bbeba5b2bc35bbe808
Akshara Sundararajan <akshara-sun@users.noreply.github.com>
2021-11-12 15:18:09 -0800

    organized files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Synth from './components/main';
+import Synth from './components/piano';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
